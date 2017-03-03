849db158a1959dfe048926956970ca1886c0c52e
Mark Greenwood <mrkjgreenwood@gmail.com>
2017-03-03 09:56:44 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const app = require('./lib/app');
+const http = require('http');
+
+const port = process.env.PORT || 3030;
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log('Server running at ', server.address().address, ':', server.address().port);
+});
